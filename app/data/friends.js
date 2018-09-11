e4a0de2cf70c13672117865501c24974f6fd1784
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-10 23:16:58 -0600

    added files

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friendsArray = [
+    {
+      customerName: "name",
+      customerEmail: "ahmed@example.com",
+      customerID: "afhaque89",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var friends = require("../data/friends")
+console.log(friends);
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+
+  app.get("/api/friends", function(req, res) {
+    res.send(friends);
+  });
+
+};
